aa256d02ba81badd8f401ad4c4cc3317ffd71f0a
Gilgihoon <87738848+Gilgihoon@users.noreply.github.com>
2021-08-02 16:15:51 +0900

    Add files via upload

---
+++ gh11.java
@@ -0,0 +1,13 @@
+
+public class gh11 {
+
+	int plus(int x, int y) {
+		int result = x+y;
+		return result;
+	}
+
+	double plus (double x, double y) {
+		double result = x*y;
+		return result;
+	}
+}
---
+++ gh12.java
@@ -0,0 +1,18 @@
+
+public class gh12 {
+
+	public static void main(String[] args) {
+		gh11 g = new gh11();
+		gh11 h = new gh11();
+		
+		int a = g.plus(2,9);
+		double b = h.plus(3.54,9.558);
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+				
+		
+	}
+
+}
